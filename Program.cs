158224d70d9dc5d149565c1270c8657ac3b8dc16
Janus Schmidt <janusschmidt@gmail.com>
2015-04-17 14:35:50 +0200

    Test autofixture
    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Ploeh.AutoFixture;
+
+namespace autofixturetest
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var fixture = new Fixture();
+      Console.WriteLine(fixture.Create<string>());
+
+      var fixture2 = new Fixture();
+      Console.WriteLine(fixture2.Create<string>());
+
+      Console.WriteLine("press key to close");
+      Console.ReadLine();
+    }
+  }
+}
